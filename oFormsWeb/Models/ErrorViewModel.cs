f80a60c547c3eead1f381b849b9a3142f570faca
Igor Ljaskevic <iljaskevic@architech.ca>
2018-03-24 23:52:15 -0400

    Add project files.

---
+++ oFormsWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace oFormsWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
